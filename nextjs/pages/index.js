92ce84d02c0742a263cf6ce742aa8ab5e6dc67a9
hongsoom <hongsoom@kakao.com>
2023-01-11 18:42:34 +0900

    [Feat] params

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -5,12 +5,7 @@ import Link from "next/link";
 export default function Home({ results }) {
   const router = useRouter();
   const onClick = (id, title) => {
-    router.push({
-      pathname :`/movies/${id}`, 
-      query : {
-        title,
-      },
-    },`/movies/${id}`);
+    router.push(`/movies/${title}/${id}`);
   };
   return (
     <div className="container">
@@ -19,12 +14,7 @@ export default function Home({ results }) {
         <div onClick={() => onClick(movie.id, movie.original_title)}className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>
-            <Link href={{
-              pathname : `/movies/${movie.id}`,
-              query : {
-                title : movie.original_title,
-              },
-            }} as={`/movies/${movie.id}`}>
+            <Link href={`/movies/${movie.original_title}/${movie.id}`}>
               {movie.original_title}
             </Link>
           </h4>
